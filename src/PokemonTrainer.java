16eda07c27b6e86765728aed5657e97d914a2260
Maarten Postma <maarten.postma1000@gmail.com>
2024-06-12 12:32:17 +0200

    Add pokemontrainer class & constructor

---
+++ src/PokemonTrainer.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class PokemonTrainer {
+  private String user;
+  private List<Pokemon> pokemonList;
+
+
+  public PokemonTrainer(String user, List<Pokemon> pokemons) {
+    this.user = user;
+    this.pokemonList = pokemons;
+  }
+}
