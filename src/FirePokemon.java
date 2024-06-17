e8eca74f453669f37206ecbec11d74cc11d1acbf
mjerta <maarten.postma1000@gmail.com>
2024-06-17 09:26:21 +0200

    Add one implementation of pyroBall

--- src/FirePokemon.java
+++ src/FirePokemon.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FirePokemon extends Pokemon {
+  // needs another variable
+  private int damage;
 
   public FirePokemon(String name, int level, int hp, String pokemonFood, String sound) {
     super(name, level, hp, pokemonFood, sound);
@@ -23,16 +25,17 @@ public class FirePokemon extends Pokemon {
 
   public void pyroBall(Pokemon name, Pokemon enemy) {
     System.out.println(name.getName() + " attacks " + enemy.getName() + " with pyroBall");
-
+    damage = 30;
+    System.out.println(enemy + " loses " + damage + " hp");
+    enemy.setHp(enemy.getHp() - damage);
+    System.out.println(enemy + " now has an hp of " + enemy.getHp());
   }
 
-  public  void fireLash(Pokemon name, Pokemon enemy) {
+  public void fireLash(Pokemon name, Pokemon enemy) {
     System.out.println(name.getName() + " attacks " + enemy.getName() + " with fireLash");
-
   }
 
   public void flameThrower(Pokemon name, Pokemon enemy) {
     System.out.println(name.getName() + " attacks " + enemy.getName() + " with flameThrower");
-
   }
 }
